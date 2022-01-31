304cf4728257cc38f511bd1e4f50fe41df4955bd
MordsDepp <89539474+MordsDepp@users.noreply.github.com>
2022-01-31 09:34:47 +0100

    Add files via upload

--- pir.py
+++ pir.py
@@ -5,38 +5,38 @@ import time
 import RPi.GPIO as io
 import subprocess
 
-io.setmode(io.BOARD)		#setup IO Pins in board mode
+io.setmode(io.BCM)		#setup IO Pins in board mode
 TIMEOUT = 30			#seconds to screen blank
 BOOTDELAY = 40			#programm delay to ensure correct boot
-PIR_PIN = 22			#PIR signal
+PIR_PIN = 25			#PIR signal
 STATE = 0
-PREV_STATE = 1
 
 io.setup(PIR_PIN, io.IN)
 
 def main():
 	time.sleep(BOOTDELAY)
-	
+	PREV_STATE = 1
 	while True:
-		STATE = io.input(PIR_PIN)
+                
+                STATE = io.input(PIR_PIN)
 		
-		if STATE==1 and PREV_STATE==0:
-			turn_on()
-			PREV_STATE = 1
-			time.sleep(TIMEOUT)
+                if STATE==1 and PREV_STATE==0:
+                    turn_on()
+                    PREV_STATE = 1
+                    time.sleep(TIMEOUT)
 			
 		
-		elif STATE==0 and PREV_STATE==1:
-			turn_off()
-			PREV_STATE = 0
+                elif STATE==0 and PREV_STATE==1:
+                    turn_off()
+                    PREV_STATE = 0
 			
 	time.sleep(.1)
 
 def turn_on():
-	subprocess.call(["xscreensaver-command", "-deactivate"])
+	subprocess.call("xscreensaver-command -deactivate", shell=True)
 
 def turn_off():
-	subprocess.call(["xscreensaver-command", "-activate"])
+	subprocess.call("xscreensaver-command -activate", shell=True)
 
 try:
 	main()
